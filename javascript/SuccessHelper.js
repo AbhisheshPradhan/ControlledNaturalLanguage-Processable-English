8f9718fe23f6dd73814c5150182769d5a512805c
Abhishesh <pradhan.abhishesh@gmail.com>
2019-07-29 13:09:08 +1000

    Refactoring done.

--- javascript/SuccessHelper.js
+++ javascript/SuccessHelper.js
@@ -1,152 +1,117 @@
 /**
 *     Contains helper functions for when AJAX requests is successful
-*     Modified by Rolf Schwitter: 2019-01-04
+*     Modified by Abhishesh Pradhan: 29/07/2019
 */
 
 
 var SuccessHelper = {
 
-      loadSingleTextFile: function(data, textStatus, jqXHR) {
-            var r = "";
-            var json = JSON.parse(data);
-            var s = json.spectext;
-            var sentence = "";
-            // FORMATS TO READABLE INPUT
-            s = s.replace(/%(.)*/g, '');
-            s = s.split('\r').join('');
-            s = s.split('\n').join('');
-            s = s.replace(/\/\*.*\*\//, '');
-            s = s.split('\r').join('');
-            s = s.split('\n').join('');
-            s = s.split('  ').join('');
-            s = s.split('.').join(' . ');
-            s = s.split('?').join(' ? ');
-            s = s.split(', ').join(' , ');
-            
+      /*
+      Formats the data to readable input
+      Filters out any other character other than alphabets and contructs an array of words
 
-            var nodes = s.split(" ");
-            
+      Input:
+            data: a large set of string containing symbols, break-points, punctuation, spaces, etc
+      Output: 
+            data: an array of words contained in the input
+      */
+      formatToReadableInput (data) {
+            data = data.replace(/%(.)*/g, '');
+            data = data.split('\r').join('');
+            data = data.split('\n').join('');
+            data = data.replace(/\/\*.*\*\//, '');
+            data = data.split('\r').join('');
+            data = data.split('\n').join('');
+            data = data.split('  ').join('');
+            data = data.split('.').join(' . ');
+            data = data.split('?').join(' ? ');
+            data = data.split(', ').join(' , ');
+            return data.split(" ");
+      },
+
+
+      /*
+      contructs sentence using nodes passed
+      add sentence to textList (CNL text list) and display
+      if mode == 1, add sentence to textLineData
+
+      Input:
+            nodes: array of words
+            mode: flag to add sentence to textLineData
+      */
+      addSentences (nodes, mode) {
+            var sentence = "";
             textLineData['sentences'] = [];
-	      viewModel.textList([]);
+            viewModel.textList([]);
 
             for (i = 0; i < nodes.length; i++) {
-                 var x = i;
-                 if (nodes[i] == "." || nodes[i] == "?") {
+                  var x = i;
+                  if (nodes[i] == "." || nodes[i] == "?") {
                         sentence = sentence.slice(0, sentence.length - 1);
                         sentence += nodes[i];
-                        textLineData.addSentence(sentence);
+                        if(mode == 1) {
+                              textLineData.addSentence(sentence);
+                        }
                         viewModel.textList.push(sentence);
                         sentence = "";
                   }
-                else {
-                    sentence+=nodes[i]+" "; }
-                i = x;
+                  else {
+                        sentence += nodes[i] + " ";
+                  }
+                  i = x;
             }
-	      //viewModel.anaExp(json.ana);
+      },
+
+      setupViewModel(json) {
             viewModel.setAsp(json);
             viewModel.setAnswer(json.answer);
             viewModel.postToken(" ");
             viewModel.$loader.css("visibility", "hidden");
       },
 
-      generateText: function(data, textStatus, jqXHR) {
-            var r = "";
+      loadSingleTextFile: function (data, textStatus, jqXHR) {
             var json = JSON.parse(data);
             var s = json.spectext;
-            var sentence = "";
-            // FORMATS TO READABLE INPUT
-            s = s.replace(/%(.)*/g, '');
-            s = s.split('\r').join('');
-            s = s.split('\n').join('');
-            s = s.replace(/\/\*.*\*\//, '');
-            s = s.split('\r').join('');
-            s = s.split('\n').join('');
-            s = s.split('  ').join('');
-            s = s.split('.').join(' . ');
-            s = s.split('?').join(' ? ');
-            s = s.split(', ').join(' , ');
-
-            var nodes = s.split(" ");
-            
-            textLineData['sentences'] = [];
-	      viewModel.textList([]);
-
-            for (i = 0; i < nodes.length; i++) {
-                 var x = i;
-                 if (nodes[i] == "." || nodes[i] == "?") {
-                        sentence = sentence.slice(0, sentence.length - 1);
-                        sentence += nodes[i];
-                        textLineData.addSentence(sentence);
-                        // viewModel.textList.push(sentence);
-                        sentence = "";
-                  }
-                else {
-                    sentence+=nodes[i]+" "; }
-                i = x;
-            }
-
-            var g = json.gentext;
-            var gsentence = "";
-            // FORMATS TO READABLE INPUT
-            g = g.replace(/%(.)*/g, '');
-            g = g.split('\r').join('');
-            g = g.split('\n').join('');
-            g = g.replace(/\/\*.*\*\//, '');
-            g = g.split('\r').join('');
-            s = g.split('\n').join('');
-            g = g.split('  ').join('');
-            g = g.split('.').join(' . ');
-            g = g.split('?').join(' ? ');
-            g = g.split(', ').join(' , ');
-            
-
-            var gnodes = g.split(" ");
-            
-            //textLineData['sentences'] = [];
-	      //viewModel.textList([]);
-
-            for (i = 0; i < gnodes.length; i++) {
-                 var x = i;
-                 if (gnodes[i] == "." || gnodes[i] == "?") {
-                        gsentence = gsentence.slice(0, gsentence.length - 1);
-                        gsentence += gnodes[i];
-                        // textLineData.addSentence(sentence);
-                        viewModel.textList.push(gsentence);
-                        gsentence = "";
-                  }
-                else {
-                    gsentence+=gnodes[i]+" "; }
-                i = x;
-            }  
-	      //viewModel.anaExp(json.ana);
-            viewModel.setAsp(json);
-            viewModel.setAnswer(json.answer);
-            viewModel.postToken(" ");
-            viewModel.$loader.css("visibility", "hidden");
-	    
+            var nodes = SuccessHelper.formatToReadableInput(s);
+            SuccessHelper.addSentences(nodes, 1);
+            //viewModel.anaExp(json.ana);
+            SuccessHelper.setupViewModel(json);
       },
 
+      generateText: function (data, textStatus, jqXHR) {
+            var json = JSON.parse(data);
+            var s = json.spectext;
+            var nodes = SuccessHelper.formatToReadableInput(s);
 
-      postTokenForLoading: function(data) {
-                  var json = JSON.parse(data);
-                  viewModel.populateLookUpTable(json);
+            SuccessHelper.addSentences(nodes, 1);
 
-                  if (word == "." || word == "?") {
-                        viewModel.setAsp(json.asp);
-                        viewModel.allowInput = true;
-                  }
+            var g = json.gentext;
+            var gnodes = SuccessHelper.formatToReadableInput(g);
 
-                  if(json.hasOwnProperty('spelling suggestions') || (json.lookahead.length == 0 && !json.hasOwnProperty('asp'))) {
-                        viewModel.allowInput = false;
-                        alert('"'+word+'" is not a valid token. Please go back and re-enter a token or add to lexicon');
-                        viewModel.addButton("Add");
-                  }
-                  else {
-                        viewModel.allowInput = true;
-                  }
+            SuccessHelper.addSentences(gnodes, 0);
+            SuccessHelper.setupViewModel(json);
+      },
 
-                  if(json.hasOwnProperty('ana') && word != ".") {
-                        viewModel.anaExp(json.ana);
-                  }
+      postTokenForLoading: function (data) {
+            var json = JSON.parse(data);
+            viewModel.populateLookUpTable(json);
+
+            if (word == "." || word == "?") {
+                  viewModel.setAsp(json.asp);
+                  viewModel.allowInput = true;
+            }
+
+            if (json.hasOwnProperty('spelling suggestions') || (json.lookahead.length == 0 && !json.hasOwnProperty('asp'))) {
+                  viewModel.allowInput = false;
+                  alert('"' + word + '" is not a valid token. Please go back and re-enter a token or add to lexicon');
+                  viewModel.addButton("Add");
+            }
+            else {
+                  viewModel.allowInput = true;
+            }
+
+            if (json.hasOwnProperty('ana') && word != ".") {
+                  viewModel.anaExp(json.ana);
+            }
       }
 }
