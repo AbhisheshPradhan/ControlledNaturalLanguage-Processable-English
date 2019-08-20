f163eb270930834cb4f407af80a03413f1bead28
Abhishesh <pradhan.abhishesh@gmail.com>
2019-08-21 09:26:04 +1000

    Add files via upload

--- prolog_server_unix.pl
+++ prolog_server_unix.pl
@@ -76,13 +76,11 @@ server(Port) :-
 %   - CSS handler
 %---------------------------------------------------------------
 
-:- http_handler('/css/styles.css', css_styles, []).
+:- http_handler('./src/css/styles.css', css_styles, []).
 
-:- http_handler('/superfish-master/dist/css/superfish.css', css_superfish, []).
+:- http_handler('./lib/boostrap/bootstrap.css', css_bootstrap, []).
 
-:- http_handler('/css/bootstrap.css', css_bootstrap, []).
-
-:- http_handler('/css/bootstrap-switch.min.css', css_bootstrap_switch, []).
+:- http_handler('./lib/boostrap/bootstrap-toggle.css', css_bootstrap_toggle, []).
 
 %---------------------------------------------------------------
 % http_handler/3
@@ -90,44 +88,14 @@ server(Port) :-
 %   - JavaScript handler
 %---------------------------------------------------------------
 
-:- http_handler('/superfish-master/dist/js/hoverIntent.js', js_hover, []).
 
-:- http_handler('/superfish-master/dist/js/superfish.js', js_superfish, []).
+:- http_handler('./lib/boostrap/bootstrap-toggle.js', js_bootstrap_toggle, []).
 
-:- http_handler('/javascript/superfish_modules.js', js_superfish_mod, []).
+:- http_handler('./src/js/ClickHelper.js', js_click_helper, []).
 
-:- http_handler('/javascript/LookaheadObject.js', js_lookahead, []).
+:- http_handler('./lib/jquery/jquery.min.js', js_jquery, []).
 
-:- http_handler('/javascript/ViewModel.js', js_view_model, []).
-
-:- http_handler('/javascript/TextArea.js', js_text_area, []).
-
-:- http_handler('/javascript/SuccessHelper.js', js_success_helper, []).
-
-:- http_handler('/javascript/ClickHelper.js', js_click_helper, []).
-
-:- http_handler('/javascript/FeatureStructure.js', js_feature_struct, []).
-
-:- http_handler('/js_library/knockout-min.js', js_knockout, []).
-
-:- http_handler('/js_library/jquery-ui.js', js_jquery_ui, []).
-
-:- http_handler('/js_library/jquery.min.js', js_jquery, []).
-
-:- http_handler('/js_library/bootstrap.min.js', js_bootstrap, []).
-
-:- http_handler('/javascript/Autocomplete.js', js_autocomplete, []).
-
-:- http_handler('/javascript/KeyEventHelper.js', js_key_helper, []).
-
-:- http_handler('/js_library/bootstrap-switch.min.js', js_bootstrap_switch, []).
-
-:- http_handler('/fonts/glyphicons-halflings-regular.woff', font1, []).
-
-:- http_handler('/fonts/glyphicons-halflings-regular.woff2', font2, []).
-
-:- http_handler('/fonts/glyphicons-halflings-regular.ttf', font3, []).
-
+:- http_handler('./lib/jquery/bootstrap.min.js', js_bootstrap, []).
 
 %---------------------------------------------------------------
 % HTTP Reply
@@ -166,7 +134,7 @@ handle(Request) :-
       reply_json(JSON)
       % display_json_out(JSON)
    ;
-      http_reply_file('html/index.html', [mime_type('text/html')], Request)
+      http_reply_file('./index.html', [mime_type('text/html')], Request)
    ).
 
 
@@ -194,82 +162,29 @@ display_json_out(JSON) :-
 %---------------------------------------------------------------
 
 css_styles(Request) :-
-  http_reply_file('css/styles.css', [mime_type('text/css')], Request).
-
-css_mic_styles(Request) :-
-  http_reply_file('css/mic-styles.css', [mime_type('text/css')], Request).
-
-css_superfish(Request) :-
-  http_reply_file('superfish-master/dist/css/superfish.css', [mime_type('text/css')], Request).
+  http_reply_file('./src/css/styles.css', [mime_type('text/css')], Request).
 
 css_bootstrap(Request) :-
-  http_reply_file('css/bootstrap.css', [mime_type('text/css')], Request).
+  http_reply_file('./lib/bootstrap/bootstrap.css', [mime_type('text/css')], Request).
 
-css_bootstrap_switch(Request) :-
-  http_reply_file('css/bootstrap-switch.min.css', [mime_type('text/css')], Request).
+css_bootstrap_toggle(Request) :-
+  http_reply_file('./lib/bootstrap/bootstrap-toggle.css', [mime_type('text/css')], Request).
 
 %---------------------------------------------------------------
 % JavaScript closure
 %---------------------------------------------------------------
 
-js_hover(Request) :-
-  http_reply_file('superfish-master/dist/js/hoverIntent.js', [mime_type('text/javascript')], Request).
-
-js_superfish(Request) :-
-  http_reply_file('superfish-master/dist/js/superfish.js', [mime_type('text/javascript')], Request).
-
-js_superfish_mod(Request) :-
-  http_reply_file('javascript/superfish_modules.js', [mime_type('text/javascript')], Request).
-
-js_lookahead(Request) :-
-  http_reply_file('javascript/LookaheadObject.js', [mime_type('text/javascript')], Request).
-
-js_view_model(Request) :-
-  http_reply_file('javascript/ViewModel.js', [mime_type('text/javascript')], Request).
-
-js_text_area(Request) :-
-  http_reply_file('javascript/TextArea.js', [mime_type('text/javascript')], Request).
-
-js_success_helper(Request) :-
-  http_reply_file('javascript/SuccessHelper.js', [mime_type('text/javascript')], Request).
+js_bootstrap_toggle(Request) :-
+  http_reply_file('./lib/bootstrap/bootstrap-toggle.js', [mime_type('text/javascript')], Request).
 
 js_click_helper(Request) :-
-  http_reply_file('javascript/ClickHelper.js', [mime_type('text/javascript')], Request).
-
-
-js_feature_struct(Request) :-
-  http_reply_file('javascript/FeatureStructure.js', [mime_type('text/javascript')], Request).
-
-js_knockout(Request) :-
-  http_reply_file('js_library/knockout-min.js', [mime_type('text/javascript')], Request).
-
-js_jquery_ui(Request) :-
-  http_reply_file('js_library/jquery-ui.js', [mime_type('text/javascript')], Request).
+  http_reply_file('./src/js/ClickHelper.js', [mime_type('text/javascript')], Request).
 
 js_jquery(Request) :-
-  http_reply_file('js_library/jquery.min.js', [mime_type('text/javascript')], Request).
+  http_reply_file('./lib/jquery/jquery.min.js', [mime_type('text/javascript')], Request).
 
 js_bootstrap(Request) :-
-  http_reply_file('js_library/bootstrap.min.js', [mime_type('text/javascript')], Request).
-
-js_autocomplete(Request) :-
-  http_reply_file('javascript/Autocomplete.js', [mime_type('text/javascript')], Request).
-
-js_key_helper(Request) :-
-  http_reply_file('javascript/KeyEventHelper.js', [mime_type('text/javascript')], Request).
-
-js_bootstrap_switch(Request) :-
-  http_reply_file('js_library/bootstrap-switch.min.js', [mime_type('text/javascript')], Request).
-
-font1(Request) :-
-   http_reply_file('fonts/glyphicons-halflings-regular.woff', [mime_type('font/opentype')], Request).
-
-font2(Request) :-
-   http_reply_file('fonts/glyphicons-halflings-regular.woff2', [mime_type('font/opentype')], Request).
-
-font3(Request) :-
-   http_reply_file('fonts/glyphicons-halflings-regular.ttf', [mime_type('font/opentype')], Request).
-
+  http_reply_file('./lib/bootstrap/bootstrap.min.js', [mime_type('text/javascript')], Request).
 
 % ---------------------------------------------------------------
 % Starts the server on port 8085
