a735c4abf5a93036aab3bb7a18843cc7988d012a
Schwitter <Rolf.Schwitter@mq.edu.au>
2019-12-09 13:51:48 +1100

    Added toggling for background theory

--- src/scripts/Results.js
+++ src/scripts/Results.js
@@ -24,3 +24,15 @@ let results = {
     viewModel.allowInput = true;
   }
 }
+
+/// for switching background theory on and off
+$("[name='my-checkbox']").bootstrapSwitch();
+$('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function(event, state) {
+      if (state) {
+            viewModel.asp(viewModel.bigAsp);
+      }
+      else {
+            viewModel.asp(viewModel.smallAsp);
+      }
+      viewModel.aspState = state;
+});
